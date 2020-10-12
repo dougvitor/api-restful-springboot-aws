1c6c6458add405348a26a6b6000c31d1d92f0adc
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2020-10-12 13:59:59 -0300

    08. Mapeamento das entidades (Mapeamento objeto relacional)

--- src/main/java/br/com/home/api/domain/RequestStage.java
+++ src/main/java/br/com/home/api/domain/RequestStage.java
@@ -2,17 +2,47 @@ package br.com.home.api.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import br.com.home.api.domain.enums.RequestState;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Entity(name = "request_stage")
 public class RequestStage {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(name = "realization_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date realizationDate;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "request_id", nullable = false)
 	private Request request;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 }
